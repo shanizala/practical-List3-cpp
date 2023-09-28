4ad2f27152d918927234785f5c084b77a79057f0
shanizala <132726720+shanizala@users.noreply.github.com>
2023-09-28 08:12:50 +0530

    p-3-5.cpp

---
+++ p-3-5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Distance {
+private:
+    int feet;
+    float inches;
+
+public:
+    void get_distance() {
+        cout<<"shani zalavadiya"<<endl;
+        cout<<"220130318007"<<endl;
+        cout << "Enter feet: ";
+        cin >> feet;
+        cout << "Enter inches: ";
+        cin >> inches;
+    }
+
+    void display_distance() {
+        cout << "Feet: " << feet << " Inches: " << inches << endl;
+    }
+};
+
+int main() {
+    Distance d1;
+
+     d1.get_distance();
+
+     d1.display_distance();
+
+   return 0;
+}
